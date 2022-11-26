ca33ce548bbf58671c35c1c2ab225c2f3a2a8ddd
Masaki Kobayashi <contact@mkobayashi.me>
2022-11-26 12:32:25 +0900

    Add simple doc page for redirection for the latest release

---
+++ bin/updateDocs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+new_version=$1
+
+if [[ -z "$new_version" ]]; then
+  echo "Pass version number." 1>&2
+  exit 1
+fi
+
+sed -i "s@https://raw.githubusercontent.com/manaba-enhanced-for-tsukuba/dist-firefox/main/versions/manaba_enhanced_for_tsukuba-.*.xpi@https://raw.githubusercontent.com/manaba-enhanced-for-tsukuba/dist-firefox/main/versions/manaba_enhanced_for_tsukuba-$new_version.xpi@" docs/latest/index.html README.md
